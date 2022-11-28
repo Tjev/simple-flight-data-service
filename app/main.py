87e007593c124c2fb2e36c038ff1cb432ac109d4
Tjev <tomasjevocin@gmail.com>
2022-11-28 03:15:05 +0100

    Extract DataProvider into separate module

--- app/main.py
+++ app/main.py
@@ -1,38 +1,12 @@
-import os
-from pathlib import Path
-from typing import Dict, List, Optional
+from typing import List, Optional
 
 import pandas as pd
 from fastapi import Depends, FastAPI
 
 from . import schemas
+from .provider import DataProvider
 
 DATA_DIR = "faa_data/"
-
-
-class DataProvider:
-    def __init__(self, data_dir: str) -> None:
-        self._load_data(data_dir)
-
-    def _load_data(self, data_dir: str) -> None:
-        df_dict: Dict[str, pd.DataFrame] = dict()
-        for file in os.scandir(data_dir):
-            if not file.is_file():
-                continue
-
-            file_path = Path(file)
-            if file_path.suffix != ".parquet":
-                continue
-
-            df_dict[file_path.stem] = pd.read_parquet(file_path)
-
-        self._df_dict = df_dict
-
-    def get_available_datasets(self) -> List[str]:
-        return list(self._df_dict.keys())
-
-    def get_dataset(self, dataset: str) -> pd.DataFrame:
-        return self._df_dict[dataset]
 
 
 app = FastAPI()
